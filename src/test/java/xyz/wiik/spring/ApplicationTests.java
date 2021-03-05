fa2ca193e47708baa20c854ed3f7ea4df1673fb7
Theo <theo.le.wiik@gmail.com>
2021-03-05 18:55:19 +0100

    Format project with 2 spaces and update to java 15

--- src/test/java/xyz/wiik/spring/ApplicationTests.java
+++ src/test/java/xyz/wiik/spring/ApplicationTests.java
@@ -2,12 +2,13 @@ package xyz.wiik.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+    Assert.isTrue(true, "It's true!");
+  }
 }
